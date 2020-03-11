c9abdafda2fba3b773788eb7029f09dcc3e4af35
11708 <1170807870@qq.com>
2020-03-11 13:02:52 +0800

    测试 firstBoot

---
+++ src/test/java/com/iafoot/firstboot/FirstbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iafoot.firstboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
